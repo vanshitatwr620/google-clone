95a52a3bdf05ea4e791b10566d96342c3ba1ec5c
Vanshita Tiwari <vanshitatwr620@gmail.com>
2021-07-27 14:50:43 +0530

    First Commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './App.css';
+import Home from './pages/Home';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import SearchPage from './pages/SearchPage';
+
+function App() {
+  return (
+    <div className="app">
+      <Router>
+
+        <Switch>
+          <Route path="/search">
+              <SearchPage />
+          </Route>
+          <Route path="/">
+              <Home />
+          </Route>
+        </Switch>
+      </Router>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/keys.js
@@ -0,0 +1,3 @@
+export const API_KEY = 'AIzaSyC0JgTK24-42AxA7WEr7ssrY64YbywGk_4'
+
+export default API_KEY;
